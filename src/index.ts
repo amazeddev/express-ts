43b26d5871fd087498d3f8ed363ce6219061708a
amazeddev <amazeddev@gmail.com>
2023-01-09 23:09:04 +0100

    prettier & linter

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,12 @@
-import express, { Request, Response } from "express";
+import express, { Request, Response } from 'express';
 
 const app = express();
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Hello World!");
+app.get('/', (req: Request, res: Response) => {
+  console.log('dsfdsg');
+  res.send('Hello World!');
 });
 
 app.listen(5000, () => {
-  console.log("Server started on port 5000!");
+  console.log('Server started on port 5000!');
 });
