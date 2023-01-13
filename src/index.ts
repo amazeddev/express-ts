6477e1214394dab8d5a01ce93ba800fb8b9321bf
amazeddev <amazeddev@gmail.com>
2023-01-13 23:01:01 +0100

    eslint & prettier

--- src/index.ts
+++ src/index.ts
@@ -1,35 +1,35 @@
-import express, { Request, Response } from "express";
+import express, { Request, Response } from 'express';
 
 const app = express();
 app.use(express.json());
 
-const randomId = () => Math.random().toString(36).slice(2, 7);
+const randomId = (): string => Math.random().toString(36).slice(2, 7);
 
-type Book = {
+interface Book {
   id: string;
   title: string;
   author: string;
   published: number;
-};
+}
 
 let books: Book[] = [
   {
     id: randomId(),
-    title: "Lód",
-    author: "Dukaj",
+    title: 'Lód',
+    author: 'Dukaj',
     published: 2007,
   },
 ];
 
-//GET all books
-app.get("/api/books", (req: Request, res: Response) => {
+// GET all books
+app.get('/api/books', (req: Request, res: Response) => {
   res.json({
     data: books,
   });
 });
 
 // POST book
-app.post("/api/books/", (req: Request, res: Response) => {
+app.post('/api/books/', (req: Request, res: Response) => {
   const { title, author, published } = req.body;
   const newBook = {
     id: randomId(),
@@ -44,28 +44,28 @@ app.post("/api/books/", (req: Request, res: Response) => {
 });
 
 // GET one book by id
-app.get("/api/books/:id", (req: Request, res: Response) => {
+app.get('/api/books/:id', (req: Request, res: Response) => {
   const { id } = req.params;
-  const book = books.find((item) => item.id === id);
+  const book = books.find(item => item.id === id);
   res.json({
     data: book,
   });
 });
 
 // DELETE book by id
-app.delete("/api/books/:id", (req: Request, res: Response) => {
+app.delete('/api/books/:id', (req: Request, res: Response) => {
   const { id } = req.params;
-  books = books.filter((user) => user.id !== id);
+  books = books.filter(user => user.id !== id);
   res.status(204);
 });
 
 // PUT (update) book by id
-app.put("/api/books/:id", (req: Request, res: Response) => {
+app.put('/api/books/:id', (req: Request, res: Response) => {
   const { id } = req.params;
   const { title, author, published } = req.body;
-  const existingBook = books.find((item) => item.id === id);
+  const existingBook = books.find(item => item.id === id);
 
-  if (!existingBook) {
+  if (existingBook === undefined) {
     const newBook = {
       id: randomId(),
       title,
@@ -80,12 +80,12 @@ app.put("/api/books/:id", (req: Request, res: Response) => {
 
   const updatedBook: Book = {
     id,
-    title: title ? title : existingBook?.title,
-    author: author ? author : existingBook?.author,
-    published: published ? published : existingBook?.published,
+    title: title === undefined ? existingBook?.title : title,
+    author: author === undefined ? existingBook?.author : author,
+    published: published === undefined ? existingBook?.published : published,
   };
 
-  books = books.map((book) => {
+  books = books.map(book => {
     if (book.id === id) {
       return updatedBook;
     } else {
@@ -99,5 +99,5 @@ app.put("/api/books/:id", (req: Request, res: Response) => {
 });
 
 app.listen(5000, () => {
-  console.log("Server started on port 5000!");
+  console.log('Server started on port 5000!');
 });
